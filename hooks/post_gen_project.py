4126d7476971edbd9b82465ec94696c44a00745b
deon <deon@aisingapore.org>
2024-02-19 15:12:19 +0800

    [feat] abstract sample problem as a cookiecutter option

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,39 @@
+import shutil
+import os
+
+
+PROBLEM_TEMPLATE = "{{cookiecutter.default_problem_template}}"
+SUB_DIRS = ["src", "conf", "dockerfiles", "notebooks"]
+
+
+def populate_problem(problem_domain: str) -> None:
+    for subdir in SUB_DIRS:
+
+        working_dir = os.getcwd()
+        src_dir = os.path.join(working_dir, "problem-templates", problem_domain)
+        shutil.copytree(
+            f"{src_dir}/{subdir}",
+            f"{working_dir}/{subdir}"
+        )
+        shutil.copyfile(
+            f"{src_dir}/{{cookiecutter.repo_name}}-conda-env.yaml",
+            f"{working_dir}/{{cookiecutter.repo_name}}-conda-env.yaml"
+        )
+
+
+def generate_template_scripts() -> None:
+
+    match PROBLEM_TEMPLATE.lower():
+        case "cv":
+            populate_problem("cv")
+        case "nlp":
+            populate_problem("nlp")
+        case "tabular":
+            populate_problem("tabular")
+        case _:
+            for subdir in SUB_DIRS:
+                os.makedirs(subdir, exist_ok=True)
+    shutil.rmtree(os.path.join(os.getcwd(), "problem-templates"))
+
+if __name__ == "__main__":
+    generate_template_scripts()
