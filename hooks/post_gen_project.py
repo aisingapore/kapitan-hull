5389dcc7cdda66464f7bcd5b664b2ef0ace5c801
Syakyr Surani <enquiries@syakyr.com>
2024-02-23 17:00:37 +0800

    Rename default_project_template to project_template, added function to post gen hook to remove unneeded banners

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,11 +2,10 @@ import shutil
 import os
 
 
-PROBLEM_TEMPLATE = "{{cookiecutter.default_problem_template}}"
-SUB_DIRS = ["src", "conf", "notebooks"]
-
-
 def populate_problem(problem_domain: str) -> None:
+
+    SUB_DIRS = ["src", "conf", "notebooks"]
+
     for subdir in SUB_DIRS:
 
         working_dir = os.getcwd()
@@ -24,9 +23,9 @@ def populate_problem(problem_domain: str) -> None:
 
 def generate_template_scripts() -> None:
 
-    global PROBLEM_TEMPLATE
+    PROBLEM_TEMPLATE = "{{cookiecutter.problem_template}}"
 
-    match PROBLEM_TEMPLATE.lower():
+    match PROBLEM_TEMPLATE:
         case "none":
             pass
         case "cv":
@@ -39,5 +38,27 @@ def generate_template_scripts() -> None:
             raise ValueError(f"{PROBLEM_TEMPLATE} is not a valid problem template.")
     shutil.rmtree(os.path.join(os.getcwd(), "problem-templates"))
 
+
+def remove_redundant_files() -> None:
+
+    PLATFORM = "{{cookiecutter.platform}}"
+    ORCH = "{{cookiecutter.orchestrator}}"
+
+    BANNER_PATH = os.path.join(
+        os.getcwd(), "aisg-context", "guide-site", "docs"
+    )
+    BANNERS = [
+        'kapitan-hull-eptg-gcp-runai-banner.png',
+        'kapitan-hull-eptg-onprem-runai-banner.png'
+    ]
+
+    # Remove all banners that doesn't have both PLATFORM and ORCH
+    [os.remove(
+        os.path.join(BANNER_PATH, x)) for x in BANNERS
+        if not (PLATFORM in x and ORCH in x
+    )]
+    
+
 if __name__ == "__main__":
     generate_template_scripts()
+    remove_redundant_files()
