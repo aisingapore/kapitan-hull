a52316bf5ad87b962b025bc9ce7f8ba1bfe0eae2
Syakyr Surani <syakyr@aisingapore.org>
2024-10-04 16:09:05 +0800

    refactor: remove not implemented options in cookiecutter prompts

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -18,10 +18,6 @@ def generate_template_scripts() -> None:
             pass
         case "cv":
             populate_problem("cv")
-        case "nlp":
-            populate_problem("nlp")
-        case "tabular":
-            populate_problem("tabular")
         case _:
             raise ValueError(f"{PROBLEM_TEMPLATE} is not a valid problem template.")
     shutil.rmtree(os.path.join(os.getcwd(), "problem-templates"))
