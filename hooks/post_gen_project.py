5b55b6a220239af1966d90aa3073352ce5dc8a6b
Augustine Teo <augustine@aisingapore.org>
2025-02-12 14:47:14 +0800

    cleanup

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,16 +1,14 @@
-import shutil
 import os
+import shutil
 
 
 def populate_problem(problem_domain: str) -> None:
-
     working_dir = os.getcwd()
     src_dir = os.path.join(working_dir, "problem-templates", problem_domain)
     shutil.copytree(src_dir, working_dir, dirs_exist_ok=True)
 
 
 def generate_template_scripts() -> None:
-
     PROBLEM_TEMPLATE = "{{cookiecutter.problem_template}}"
 
     match PROBLEM_TEMPLATE:
@@ -26,33 +24,26 @@ def generate_template_scripts() -> None:
 
 
 def remove_redundant_files() -> None:
-
     PLATFORM = "{{cookiecutter.platform}}"
     ORCH = "{{cookiecutter.orchestrator}}"
 
-    BANNER_PATH = os.path.join(
-        os.getcwd(), "aisg-context", "guide-site", "docs"
-    )
+    BANNER_PATH = os.path.join(os.getcwd(), "aisg-context", "guide-site", "docs")
     BANNERS = [
-        'kapitan-hull-eptg-gcp-runai-banner.png',
-        'kapitan-hull-eptg-onprem-runai-banner.png'
+        "kapitan-hull-eptg-gcp-runai-banner.png",
+        "kapitan-hull-eptg-onprem-runai-banner.png",
     ]
 
     # Remove all banners that doesn't have both PLATFORM and ORCH
-    for path, item in zip(
-        [BANNER_PATH],
-        [BANNERS]
-    ):
-        [os.remove(
-            os.path.join(path, x)) for x in item
-            if not (PLATFORM in x and ORCH in x
-        )]
-    
+    for path, item in zip([BANNER_PATH], [BANNERS]):
+        [
+            os.remove(os.path.join(path, x))
+            for x in item
+            if not (PLATFORM in x and ORCH in x)
+        ]
+
     # Remove runai yaml files if ORCH != runai
-    RUNAI_YAML_PATH = os.path.join(
-        os.getcwd(), "aisg-context", "runai"
-    )
-    if ORCH != 'runai':
+    RUNAI_YAML_PATH = os.path.join(os.getcwd(), "aisg-context", "runai")
+    if ORCH != "runai":
         shutil.rmtree(RUNAI_YAML_PATH)
 
 
