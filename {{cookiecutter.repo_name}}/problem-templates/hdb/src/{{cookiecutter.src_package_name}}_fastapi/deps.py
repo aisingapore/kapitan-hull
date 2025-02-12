d4649f4511d6265addc85dda256f1039effb25a0
Augustine Teo <augustine@aisingapore.org>
2025-02-12 18:16:39 +0800

    tweaks: xgboost does not support MPS

--- {{cookiecutter.repo_name}}/problem-templates/hdb/src/{{cookiecutter.src_package_name}}_fastapi/deps.py
+++ {{cookiecutter.repo_name}}/problem-templates/hdb/src/{{cookiecutter.src_package_name}}_fastapi/deps.py
@@ -2,8 +2,7 @@
 import {{cookiecutter.src_package_name}} as {{cookiecutter.src_package_name_short}}
 import {{cookiecutter.src_package_name}}_fastapi as {{cookiecutter.src_package_name_short}}_fapi
 
-PRED_MODEL, DEVICE = {{cookiecutter.src_package_name_short}}.modeling.utils.load_model(
+PRED_MODEL = {{cookiecutter.src_package_name_short}}.modeling.utils.load_model(
     {{cookiecutter.src_package_name_short}}_fapi.config.SETTINGS.PRED_MODEL_PATH,
     {{cookiecutter.src_package_name_short}}_fapi.config.SETTINGS.USE_CUDA,
-    {{cookiecutter.src_package_name_short}}_fapi.config.SETTINGS.USE_MPS,
 )
