c35740e5f5fdbab213e4bea74031c5c22811f7c0
Augustine Teo <augustine@aisingapore.org>
2025-02-12 18:16:19 +0800

    tweaks to use cuda if use set use_cuda to true in config.py

--- {{cookiecutter.repo_name}}/problem-templates/hdb/src/{{cookiecutter.src_package_name}}/modeling/utils.py
+++ {{cookiecutter.repo_name}}/problem-templates/hdb/src/{{cookiecutter.src_package_name}}/modeling/utils.py
@@ -4,7 +4,7 @@ import {{cookiecutter.src_package_name}} as {{cookiecutter.src_package_name_shor
 import xgboost as xgb
 
 
-def load_model(path_to_model: str) -> xgb.XGBRegressor:
+def load_model(path_to_model: str, use_cuda: bool) -> xgb.XGBRegressor:
     """Load trained model.
     In this example, we are using XGBoost, modify this method to suit your
     models.
@@ -16,5 +16,7 @@ def load_model(path_to_model: str) -> xgb.XGBRegressor:
     """
     loaded_model = xgb.XGBRegressor()
     loaded_model.load_model(path_to_model)
+    if use_cuda:
+        loaded_model.set_params(device="cuda", tree_method="hist")
 
     return loaded_model
